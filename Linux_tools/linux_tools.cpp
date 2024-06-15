466f482db566a3d15ad6584de1abd85b94da8189
Vadkon07 <vadkonbeats@gmail.com>
2024-06-15 09:22:15 +0000

    Fixed bugs

--- Linux_tools/linux_tools.cpp
+++ Linux_tools/linux_tools.cpp
@@ -8,8 +8,7 @@
 
 //Customization Guide: scrool this code down and find some lines named as 'if (number == <number>');, and inside them just insted of 'free' for e.g., insert any your app or command execution name (even fastfetch).
 //
-// BUG: AFTER COMMANDS HE SHOWS 'WRONG NUMBER', FOR NOW I DON'T KNOW HOW TO SOLVE IT. I ONLY KNOW THAT THE REASON OF IT IS LOOP.
-
+//
 using namespace std;
 
 int main() {
@@ -17,6 +16,9 @@ int main() {
 	int number;
 	
 	while (TRUE){
+
+	back:
+	
 	cout << " \n";
 	cout << "--- Linux Fast Tools --- \n";
 	cout << " \n";
@@ -39,6 +41,9 @@ int main() {
 		printf("This code is open source, and he is created by Vadkon. If you found any bugs, send an email on vadkonbeats@gmail.com (better if you found a solution for it). \n");
 		printf(" \n");
 		printf("Originally this code is adapted for Arch Linux, and I think that it's possible that some commands and file locations can be different (you can check code and adapt if for your needs and distributions. \n");
+		{ goto back;
+		}
+
 	}
 
 	if (number == 1) {
@@ -56,8 +61,8 @@ int main() {
 		printf(" \n");
 		printf("*Linux Command is 'cat /proc/cpuinfo");
 	}
-
-	else {
+       
+	if (number > 3) {
 		printf(" \n");
 		printf("\033[31mWrong number!\n");
 		printf("\033[0m\n");
